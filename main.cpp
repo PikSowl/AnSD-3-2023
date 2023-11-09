f4bc51d3eecc630905b6532913efd0bc5a3bacaf
PikSowl <kmazalov11@gmai.com>
2023-11-09 19:16:39 +0200

    GG?

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#define RusLoc _setmode(_fileno(stdout), _O_U16TEXT); _setmode(_fileno(stdin),  _O_U16TEXT); _setmode(_fileno(stderr), _O_U16TEXT)
+
+#include <iostream>
+#include <fcntl.h>
+
+using std::wcout;
+using std::wcin;
+using std::swap;
+using std::endl;
+
+void tref(int y, int &x){
+    RusLoc;
+    if (y > x) return;
+    wcout << y << " ";
+    tref((y*3), x);
+    tref((y*5), x);
+    tref((y*7), x);
+}
+
+int main() {
+    RusLoc;
+
+    int x = 100;
+    wcout << L"Введите x" << endl;
+    wcin >> x;
+    tref(1, x);
+    wcout << endl;
+    return 0;
+}
