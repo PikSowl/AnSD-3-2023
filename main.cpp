f574ddade3dae6df6a38334df2adad9936caeaf8
PikSowl <kmazalov11@gmai.com>
2023-11-13 12:53:39 +0200

    Now works

--- main.cpp
+++ main.cpp
@@ -8,13 +8,11 @@ using std::wcin;
 using std::swap;
 using std::endl;
 
-void tref(int y, int &x){
+void self(int y, int &x) {
     RusLoc;
     if (y > x) return;
     wcout << y << " ";
-    tref((y*3), x);
-    duo((y*5), x);
-    self((y*7), x);
+    self((y * 7), x);
 }
 
 void duo(int y, int &x){
@@ -25,11 +23,13 @@ void duo(int y, int &x){
     self((y*7), x);
 }
 
-void self(int y, int &x){
+void tref(int y, int &x){
     RusLoc;
     if (y > x) return;
     wcout << y << " ";
-    selff((y*7), x);
+    tref((y*3), x);
+    duo((y*5), x);
+    self((y*7), x);
 }
 
 int main() {
